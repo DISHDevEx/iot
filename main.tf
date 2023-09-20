54042c9396ceebc0ce28000165e36427d7ebcfa6
Chava <dheeraj.chava@dish.com>
2023-09-20 09:43:53 -0600

    IAM role using TF

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_iam_role" "IOT_role" {
+  name = var.name
+
+  assume_role_policy = var.assume_role_policy
+}
+
+
+resource "aws_iam_role_policy_attachment" "IOT_attachment" {
+  role       = aws_iam_role.IOT_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+
