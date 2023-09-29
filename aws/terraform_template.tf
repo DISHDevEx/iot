dd0c17d4f42e58ac9f5c043952f9771592831264
EC2 Default User <ec2-user@ip-10-0-103-92.ec2.internal>
2023-09-29 15:25:10 +0000

    updated module terragrunt files, renamed template file

---
+++ aws/terraform_template.tf
@@ -0,0 +1,9 @@
+/*
+Terraform root configuration - Includes modules related configuration
+*/
+
+#Modules
+module "ec2-instance" {
+  source = "/home/ec2-user/iot/aws/modules/ec2"
+  vars_file = "/home/ec-user/iot/aws/terraform.tfvars"
+}
