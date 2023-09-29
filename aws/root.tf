1bd51eb53b89b152c2b594311b1a696e470c289c
EC2 Default User <ec2-user@ip-10-0-103-12.ec2.internal>
2023-09-29 16:43:46 +0000

    updated file paths

--- aws/root.tf
+++ aws/root.tf
@@ -4,6 +4,6 @@ Terraform root configuration - Includes modules related configuration
 
 #Modules
 module "ec2-instance" {
-  source = "/home/ec2-user/iot/aws/modules/ec2"
-  vars_file = "/home/ec2-user/iot/aws/terraform.tfvars"
+  source = "./modules/ec2"
+  vars_file = "./terraform.tfvars"
 }
