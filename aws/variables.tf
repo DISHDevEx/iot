d36d8d187f5564fea26ae6e15a880719aee517af
EC2 Default User <ec2-user@ip-10-0-103-12.ec2.internal>
2023-10-02 19:00:44 +0000

    added sensitive flag to variables

--- aws/variables.tf
+++ aws/variables.tf
@@ -8,16 +8,19 @@ variable "aws_access_key" {
   description = "AWS account access key"
   type        = string
   default     = null
+  sensitive   = true
 }
 variable "aws_secret_key" {
   description = "AWS account secret key"
   type        = string
   default     = null
+  sensitive   = true
 }
 variable "aws_session_token" {
   description = "AWS account session token"
   type        = string
   default     = null
+  sensitive   = true
 }
 variable "vault_address" {
   description = "Vault server address"
