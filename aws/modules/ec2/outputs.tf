8852849a2bd980346154236331b303d47bedac35
EC2 Default User <ec2-user@ip-10-0-103-31.ec2.internal>
2023-10-05 16:13:42 +0000

    updated output values

--- aws/modules/ec2/outputs.tf
+++ aws/modules/ec2/outputs.tf
@@ -1,7 +1,4 @@
 #Outputs
-output "instance-id-and-public-ip" {
-  value = zipmap(aws_instance.ec2[*].id, aws_instance.ec2[*].public_ip)
-}
-output "instance-id-and-private-ip" {
-  value = zipmap(aws_instance.ec2[*].id, aws_instance.ec2[*].private_ip)
+output "instance_id" {
+  value = aws_instance.ec2[*].id
 }
