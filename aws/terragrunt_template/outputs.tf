239cf9e3c7119d27a493820c1da355df2310865b
EC2 Default User <ec2-user@ip-10-0-103-31.ec2.internal>
2023-10-06 17:15:23 +0000

    updated description

--- aws/terragrunt_template/outputs.tf
+++ aws/terragrunt_template/outputs.tf
@@ -1,6 +1,6 @@
 #Outputs
 #ec2_instance - module output
-output "ec2_instance_id" {
-  description = "ID of the EC2 instance"
+output "ec2_instance_id_and_name" {
+  description = "ID and Name of the EC2 instance"
   value       = module.ec2_instance[*]
 }
