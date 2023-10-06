f96ba36f12f6ceaa889b61c715701eb59984c8ff
EC2 Default User <ec2-user@ip-10-0-103-31.ec2.internal>
2023-10-06 17:04:49 +0000

    updated output value

--- aws/modules/ec2/outputs.tf
+++ aws/modules/ec2/outputs.tf
@@ -1,8 +1,5 @@
 #Outputs
 output "instance_id_and_name" {
   description = "ID of the EC2 instance"
-  value = {
-    id   = aws_instance.ec2[*].id,
-    name = aws_instance.ec2[*].tags.Name
-  }
+  value       = { for index in aws_instance.ec2[*] : index.id => index.Name }
 }
