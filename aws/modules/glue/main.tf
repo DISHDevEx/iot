12750cc77d14e9ededfdaa4f16b73a82d7067340
Bhumireddy <rishvita.bhumireddy@dish.com>
2023-10-10 11:32:20 -0600

    rebasing with main'

--- aws/modules/glue/main.tf
+++ aws/modules/glue/main.tf
@@ -1,36 +1,8 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 4.0"
-#     }
-#   }
-# }
-
-# provider "aws" {
-#   region = var.aws_region
-# }
-
-#resources for IAM Role
-
-# resource "aws_iam_role" "IOT_role" {
-#   name = var.name
-
-#   assume_role_policy = var.assume_role_policy
-# }
-
-
-# resource "aws_iam_role_policy_attachment" "IOT_attachment" {
-#   role       = aws_iam_role.IOT_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-# }
-
 
 # Resources for Glue
 resource "aws_glue_job" "IOT_glue_job" {
   count = length(var.job_names)
   name = format("iot_%s", var.job_names[count.index])
-  # name = var.name
   role_arn = var.role_arn
   connections = var.connections 
   description = var.description
