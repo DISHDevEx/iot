9b7bfc852fa7873e67ce26132e60a7bbbbb5f2c5
EC2 Default User <ec2-user@ip-10-0-103-31.ec2.internal>
2023-10-12 19:30:57 +0000

    remove extra spaces

--- aws/modules/s3/main.tf
+++ aws/modules/s3/main.tf
@@ -20,7 +20,6 @@ resource "aws_s3_bucket_versioning" "bucket_versioning" {
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "server_side_encryption" {
   bucket = aws_s3_bucket.s3.id
-
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256" #Server-side encryption with Amazon S3 managed keys (SSE-S3)
