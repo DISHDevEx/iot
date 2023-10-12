5fb7d37d411ef6bb5a2b61552ebcd30d092cc451
EC2 Default User <ec2-user@ip-10-0-103-31.ec2.internal>
2023-10-12 18:38:12 +0000

    updated outputs

--- aws/modules/s3/outputs.tf
+++ aws/modules/s3/outputs.tf
@@ -1,17 +1,5 @@
 #Outputs
-output "s3_bucket_name" {
-  description = "S3 bucket name"
-  value       = aws_s3_bucket.s3.id
-}
-output "s3_bucket_region" {
-  description = "S3 bucket name"
-  value       = aws_s3_bucket.s3.region
-}
-output "s3_object_versioning" {
-  description = "S3 bucket - Object versioning"
-  value       = aws_s3_bucket_versioning.object_versioning.versioning_configuration
-}
-output "s3_object_encryption" {
-  description = "S3 bucket - Object encyption"
-  value       = aws_s3_bucket_server_side_encryption_configuration.object_encryption
+output "s3_bucket_name_and_properties" {
+  description = "S3 bucket name and other properties"
+  value       = aws_s3_bucket.s3[*]
 }
