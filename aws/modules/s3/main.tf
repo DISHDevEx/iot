923672592fca94e60dbcdf9664afdaee8699a226
EC2 Default User <ec2-user@ip-10-0-103-31.ec2.internal>
2023-10-12 19:38:00 +0000

    updated policy variable

--- aws/modules/s3/main.tf
+++ aws/modules/s3/main.tf
@@ -29,7 +29,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "server_side_encry
 
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = aws_s3_bucket.s3.id
-  policy = sensitive(file(var.bucket_policy_file_path))
+  policy = file(var.bucket_policy_file_path)
 }
 
 /*
