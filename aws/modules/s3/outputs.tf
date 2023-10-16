5a7f2d5d4cc8777f04221afc73fa66423c2dafa9
EC2 Default User <ec2-user@ip-10-0-103-128.ec2.internal>
2023-10-16 21:41:43 +0000

    checking with for and if condition

--- aws/modules/s3/outputs.tf
+++ aws/modules/s3/outputs.tf
@@ -5,6 +5,7 @@ output "s3_bucket_name_and_properties" {
     bucket_name            = aws_s3_bucket.s3.bucket
     bucket_region          = aws_s3_bucket.s3.region
     bucket_versioning      = aws_s3_bucket_versioning.bucket_versioning.versioning_configuration[0].status
-    server_side_encryption = aws_s3_bucket_server_side_encryption_configuration.server_side_encryption.rule
+    server_side_encryption = [for obj in aws_s3_bucket_server_side_encryption_configuration.server_side_encryption.rule : obj if obj.sse_algorithm != null]
+    #server_side_encryption = aws_s3_bucket_server_side_encryption_configuration.server_side_encryption.rule
   }
 }
