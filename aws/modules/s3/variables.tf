e6a054df06e6160d05dae9d9c57b56049e0f995d
EC2 Default User <ec2-user@ip-10-0-103-128.ec2.internal>
2023-10-16 16:04:34 +0000

    added default policy file, update comments in s3 - main, variables

--- aws/modules/s3/variables.tf
+++ aws/modules/s3/variables.tf
@@ -9,12 +9,6 @@ variable "bucket_versioning" {
   type        = string
   default     = "Enabled"
 }
-variable "aws_account_id" {
-  description = "AWS account id to be used in the S3 bucket policy - Principal"
-  type        = number
-  default     = null
-  sensitive   = true
-}
 variable "bucket_policy_file_path" {
   description = "S3 bucket policy file path"
   type        = string
