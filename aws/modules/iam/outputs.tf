41fe377d0be26ea7904590f1f6113a8c57d02fac
EC2 Default User <ec2-user@ip-10-0-103-128.ec2.internal>
2023-10-26 17:29:20 +0000

    updated outputs for iam and glue modules

--- aws/modules/iam/outputs.tf
+++ aws/modules/iam/outputs.tf
@@ -2,7 +2,7 @@ output "iam_role_name" {
   description = "IAM role name"
   value       = aws_iam_role.IOT_role.name
 }
-#
+
 output "iam_role_arn" {
   description = "ARN of IAM role"
   value       = aws_iam_role.IOT_role.arn
