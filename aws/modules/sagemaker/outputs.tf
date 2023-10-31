40b237cfd1032b85eb5fe70779c13ad17f82d166
Sri Harsha Vadlamudi <sriharsha219@gmail.com>
2023-10-31 13:06:51 -0600

    reverted format string changes

--- aws/modules/sagemaker/outputs.tf
+++ aws/modules/sagemaker/outputs.tf
@@ -1,5 +1,5 @@
 # sagemaker - module output
 output "sagemaker_outputs" {
   description = "Sagemaker notebook instance name"
-  value       = format("Sagemaker notebook instance name = ",aws_sagemaker_notebook_instance.sagemaker_notebook_instance[*].name)
+  value       = aws_sagemaker_notebook_instance.sagemaker_notebook_instance[*].name
 }
