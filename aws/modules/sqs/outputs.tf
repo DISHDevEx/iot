028351d2ef498f629c63c9e47edbad9879072c97
Sri Harsha Vadlamudi <sriharsha219@gmail.com>
2023-10-31 13:56:10 -0600

    added string concatenation to output values

--- aws/modules/sqs/outputs.tf
+++ aws/modules/sqs/outputs.tf
@@ -1,4 +1,4 @@
 output "sqs_queue_id" {
   description = "The ID of the created sqs queue "
-  value       = { for k, v in aws_sqs_queue.iot_terraform_queue : k => v.id }
+  value       = "Index" + { for k, v in aws_sqs_queue.iot_terraform_queue : k => v.id }
 }
