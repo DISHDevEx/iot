0e09b7c0447e5871a1605f6b310e0835426b330e
RABhatty <rizwanabidbhatty@gmail.com>
2023-10-31 14:00:24 -0600

    add helm module

--- aws/terragrunt_template/main.tf
+++ aws/terragrunt_template/main.tf
@@ -194,7 +194,7 @@ module "lambda_function" {
 
 ##Lambda Function using new role with new policies
 #module "lambda_function" {
-#  source                                = "git@github.com:DISHDevEx/iot.git//aws/modules/lambda_function"
+source                                = "git@github.com:DISHDevEx/iot.git//aws/modules/lambda_function"
 #  lambda_function_name                  = "tg_test_lambda"
 #  filepath                              = "./index.py.zip"
 #  handler                               = "index.handler"
@@ -203,7 +203,7 @@ module "lambda_function" {
 #  lambda_role_name                      = "test-role"
 #  flag_use_existing_policy              = false
 #  iam_policy_name                       = "test-policy"
-#  new_iam_policy                        = <<-EOT
+#  new_iam_policy                            = <<-EOT
 #    {
 #      "Version": "2012-10-17",
 #      "Statement": [
@@ -235,7 +235,6 @@ module "lambda_function" {
 #      ]
 #    }
 #  EOT
-#  permission_boundary = "arn:aws:iam::064047601590:policy/TaaSAdminDev_Permission_Boundary"
 #}
 
 ##Lambda Function using new role with existing policies
@@ -250,7 +249,6 @@ module "lambda_function" {
 #  flag_use_existing_policy              = true
 #  policy_count                          = 2
 #  existing_iam_policy_arns              = ["arn:aws:iam::aws:policy/CloudWatchLogsFullAccess", "arn:aws:iam::aws:policy/CloudWatchFullAccess"]
-#  permission_boundary                   = "arn:aws:iam::064047601590:policy/TaaSAdminDev_Permission_Boundary"
 #}
 
 # VPC module
@@ -361,3 +359,14 @@ module "sqs" {
 #   subnet_id                 = data.vault_generic_secret.getsecrets.data["subnet_id"]
 #   vpc_security_group_ids    = [data.vault_generic_secret.getsecrets.data["vpc_security_group_ids"]]
 # }
+
+module "helm" {
+  source                    = "git@github.com:DISHDevEx/iot.git//aws/modules/helm?ref=rizwan/helm"
+  app = {
+    deploy = true
+    name = "nginx"
+    repository = "https://charts.bitnami.com/bitnami"
+    chart = "nginx"
+    namespace = "test"
+  }
+}
