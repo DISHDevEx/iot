6fa83f6af4e70619c48a759324557e31a5116caf
Sri Harsha Vadlamudi <sriharsha219@gmail.com>
2023-11-01 12:14:30 -0600

    removed unused data-source

--- aws/terragrunt_template/data-sources.tf
+++ aws/terragrunt_template/data-sources.tf
@@ -5,14 +5,3 @@ data "vault_generic_secret" "getsecrets" {
   path = var.vault_secrets_path
 }
 */
-
-
-# Data source to get the IAM role ARN created by the "iam" module
-# data "terraform_remote_state" "iam" {
-#   backend = "s3"
-#   config = {
-#     bucket = "sriharsha-iot"
-#     key    = "mss/rish/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
